b04dfa33cab4be3c594a7757f0728cf0091f8c3b
Román Martínez <rgmf@riseup.net>
2024-07-01 18:41:09 +0200

    Upload media to album: created UploaderFile class and duplicated files handle.

--- app/routers/albums.py
+++ app/routers/albums.py
@@ -1,7 +1,10 @@
+import logging
+
 from datetime import datetime, UTC
 
 from fastapi import APIRouter, status, Depends, UploadFile
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
 from app.auth.auth import get_auth_user
 from app.database.database import get_db
@@ -14,7 +17,9 @@ from app.database.crud import (
     create_album as crud_create_album,
     create_media as crud_create_media
 )
-from app.tasks.files import upload_file, read_exif, Exif, UploadFileResult
+from app.tasks.files import FileUploader
+
+logging.basicConfig(level=logging.DEBUG)
 
 router = APIRouter(
     prefix="/albums",
@@ -59,25 +64,34 @@ async def create_media(
     )
 
     for file in files:
-        result: UploadFileResult | None = await upload_file(file)
-        if result:
-            exif: Exif = await read_exif(result.relative_file_path)
-            media_create = MediaCreate(
-                name=str(file.filename),
-                hash=result.hash_base64,
-                data=result.relative_file_path,
-                thumbnail=result.relative_file_path,
-                created_at=datetime.now(UTC),
-                updated_at=datetime.now(UTC),
-                size=result.file_size,
-                media_created=exif.date_time_original_dt,
-                mime_type=result.mime_type,
-                media_type=result.media_type,
-                latitude=exif.latitude_decimal_degrees,
-                longitude=exif.longitude_decimal_degrees
-            )
-            media_upload_out.data.valid.append(crud_create_media(db, media_create))
+        file_uploader = FileUploader(file)
+        await file_uploader.upload()
+
+        if not file_uploader.has_error():
+            try:
+                media_create = MediaCreate(
+                    name=str(file.filename),
+                    hash=file_uploader.file_hash,
+                    data=file_uploader.relative_file_path,
+                    thumbnail=file_uploader.relative_file_path,
+                    created_at=datetime.now(UTC),
+                    updated_at=datetime.now(UTC),
+                    size=file_uploader.file_size,
+                    media_created=file_uploader.exif().date_time_original_dt,
+                    mime_type=file_uploader.mime_type,
+                    media_type=file_uploader.media_type,
+                    latitude=file_uploader.exif().latitude_decimal_degrees,
+                    longitude=file_uploader.exif().longitude_decimal_degrees
+                )
+                media_upload_out.data.valid.append(crud_create_media(db, media_create))
+            except IntegrityError as e:
+                media_upload_out.data.invalid.append(
+                    f"Error uploading {file.filename}: the file already exists"
+                )
+                logging.debug(f"Error uploading {file.filename}: the file already exists: {e}")
         else:
-            media_upload_out.data.invalid.append(f"{file.filename} is not a valid and accepted media file")
+            media_upload_out.data.invalid.append(
+                f"Error uploading {file.filename} file: {file_uploader.error()}"
+            )
 
     return media_upload_out
--- app/tasks/files.py
+++ app/tasks/files.py
@@ -1,17 +1,16 @@
 import os
-import base64
 import logging
 
 import exifread
+import imagehash
 
 from PIL import Image
 from pathlib import Path
-from datetime import datetime, UTC, date
+from datetime import datetime, date
 from dataclasses import dataclass, field
 from fractions import Fraction
 
 from fastapi import UploadFile
-from passlib.context import CryptContext
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -43,26 +42,17 @@ SUPPORTED_FORMATS = {
 
 
 @dataclass
-class UploadFileResult:
-    relative_file_path: str
-    hash_base64: str
-    file_size: float
-    mime_type: str
-    media_type: str
-
-
-@dataclass
 class Exif:
     date_time_original_dt: datetime | None = field(init=False)
-    date_time_original: exifread.classes.IfdTag | None
+    date_time_original: exifread.classes.IfdTag | None = None
 
     latitude_decimal_degrees: float | None = field(init=False)
-    latitude_ref: exifread.classes.IfdTag | None
-    latitude: exifread.classes.IfdTag | None
+    latitude_ref: exifread.classes.IfdTag | None = None
+    latitude: exifread.classes.IfdTag | None = None
 
     longitude_decimal_degrees: float | None = field(init=False)
-    longitude_ref: exifread.classes.IfdTag | None
-    longitude: exifread.classes.IfdTag | None
+    longitude_ref: exifread.classes.IfdTag | None = None
+    longitude: exifread.classes.IfdTag | None = None
 
     def __post_init__(self):
         self.__compute_date_time_original()
@@ -115,85 +105,123 @@ def convert_to_decimal_degrees(values: list[float, float, float]) -> float:
     return decimal_degrees
 
 
-def generate_base64_hash(text: str) -> str:
-    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    combined_string = f"{text}{datetime.now(UTC).isoformat()}"
-    hash = pwd_context.hash(combined_string)
-    return base64.urlsafe_b64encode(hash.encode("utf-8")).decode("utf-8")
+class FileUploader:
+    def __init__(self, file: UploadFile):
+        Path(TMP_PATH).mkdir(parents=True, exist_ok=True)
 
+        self.__file: UploadFile = file
+        self.__tmp_file_path: str = os.path.join(TMP_PATH, file.filename)
+        self.__relative_dir_path: str = os.path.join(MEDIAS_DIR_NAME, date.today().isoformat())
+        self.__absolute_dir_path: str = os.path.join(BASE_UPLOAD_DIR, self.__relative_dir_path)
 
-async def upload_file(file: UploadFile) -> UploadFileResult | None:
-    Path(TMP_PATH).mkdir(parents=True, exist_ok=True)
+        self.__absolute_file_path: str | None = None
+        self.relative_file_path: str | None = None
+        self.file_hash: str | None = None
+        self.file_size: float | None = None
+        self.mime_type: str | None = None
+        self.media_type: str = IMAGE_FORMAT_KEY
+        self.__exif: Exif | None = None
 
-    tmp_file_path: str = os.path.join(TMP_PATH, file.filename)
-    with open(tmp_file_path, "wb") as fo:
-        chunk: bytes = await file.read(10_000)
-        while chunk:
-            fo.write(chunk)
-            chunk = await file.read(10_000)
+        self.__error: str | None = None
 
-    try:
-        with Image.open(tmp_file_path) as im:
-            relative_dir_path = os.path.join(MEDIAS_DIR_NAME, date.today().isoformat())
-            dst_path = os.path.join(BASE_UPLOAD_DIR, relative_dir_path)
-            Path(dst_path).mkdir(parents=True, exist_ok=True)
+    async def upload(self):
+        await self.__upload_tmp_file()
+        if self.has_error():
+            return
 
-            hash_base64 = generate_base64_hash(relative_dir_path)
+        await self.__process_image()
+        if self.has_error():
+            return
 
-            extension = file.filename.split(".")[-1]
+        await self.__read_exif()
 
-            relative_file_path = os.path.join(relative_dir_path, f"{hash_base64}.{extension}")
-            absolute_file_path = os.path.join(BASE_UPLOAD_DIR, relative_file_path)
-            Path(tmp_file_path).rename(Path(absolute_file_path))
+    def cleanup(self):
+        try:
+            Path(self.__tmp_file_path).unlink()
+        except FileNotFoundError:
+            pass
 
-            file_size = Path(absolute_file_path).stat().st_size
-            format = im.format.lower()
-            mime_type = (
-                SUPPORTED_FORMATS[IMAGE_FORMAT_KEY][format]
-                if format in SUPPORTED_FORMATS[IMAGE_FORMAT_KEY]
-                else SUPPORTED_FORMATS[UNKNOWN_FORMAT_KEY]
-            )
+    def has_error(self) -> bool:
+        return self.__error is not None
 
-            return UploadFileResult(
-                relative_file_path=relative_file_path,
-                hash_base64=hash_base64,
-                file_size=file_size,
-                mime_type=mime_type,
-                media_type=IMAGE_FORMAT_KEY
-            )
+    def error(self) -> str:
+        return self.__error if self.__error is not None else ""
 
-    except OSError as error:
-        logging.debug(f"'{tmp_file_path}' is not an image: {error}")
-        Path(tmp_file_path).unlink()
+    def exif(self) -> Exif:
+        return self.__exif if self.__exif is not None else Exif()
 
-    return None
+    async def __upload_tmp_file(self):
+        try:
+            with open(self.__tmp_file_path, "wb") as fo:
+                chunk: bytes = await self.__file.read(10_000)
+                while chunk:
+                    fo.write(chunk)
+                    chunk = await self.__file.read(10_000)
+        except OSError as error:
+            self.__error = f"Error uploading {self.__file.filename}."
+            logging.debug(f"{self.__error}: {error}")
+        except Exception as error:
+            logging.error(f"Error saving temp file '{self.__tmp_file_path}': {error}")
 
+    async def __process_image(self):
+        try:
+            with Image.open(self.__tmp_file_path) as im:
+                self.file_hash = str(imagehash.phash(im))
 
-async def read_exif(relative_file_path: str) -> Exif:
-    date_time_original = None
-    latitude = None
-    longitude = None
-    latitude_ref = None
-    longitude_ref = None
+                Path(self.__absolute_dir_path).mkdir(parents=True, exist_ok=True)
 
-    with open(os.path.join(BASE_UPLOAD_DIR, relative_file_path), "rb") as fo:
-        tags = exifread.process_file(fo)
+                extension = self.__file.filename.split(".")[-1]
 
-        if "Image DateTimeOriginal" in tags:
-            date_time_original = tags["Image DateTimeOriginal"]
-        elif "Image DateTime" in tags:
-            date_time_original = tags["Image DateTime"]
+                self.relative_file_path = os.path.join(self.__relative_dir_path, f"{self.file_hash}.{extension}")
+                self.__absolute_file_path = os.path.join(BASE_UPLOAD_DIR, self.relative_file_path)
+                Path(self.__tmp_file_path).rename(Path(self.__absolute_file_path))
 
-        if "GPS GPSLatitude" in tags and "GPS GPSLongitude" in tags and "GPS GPSLatitudeRef" in tags and "GPS GPSLongitudeRef" in tags:
-            latitude = tags["GPS GPSLatitude"]
-            longitude = tags["GPS GPSLongitude"]
-            latitude_ref = tags["GPS GPSLatitudeRef"]
-            longitude_ref = tags["GPS GPSLongitudeRef"]
+                self.file_size = Path(self.__absolute_file_path).stat().st_size
+                format = im.format.lower()
+                self.mime_type = (
+                    SUPPORTED_FORMATS[IMAGE_FORMAT_KEY][format]
+                    if format in SUPPORTED_FORMATS[IMAGE_FORMAT_KEY]
+                    else SUPPORTED_FORMATS[UNKNOWN_FORMAT_KEY]
+                )
+        except OSError as error:
+            self.__error = f"'{self.__tmp_file_path}' is not an image: {error}"
+            logging.debug(self.__error)
+        except Exception as error:
+            logging.error(f"Error uploading file '{self.__tmp_file_path}': {error}")
 
-    return Exif(
-        date_time_original=date_time_original,
-        latitude=latitude,
-        longitude=longitude,
-        latitude_ref=latitude_ref,
-        longitude_ref=longitude_ref
-    )
+    async def __read_exif(self):
+        date_time_original = None
+        latitude = None
+        longitude = None
+        latitude_ref = None
+        longitude_ref = None
+
+        try:
+            with open(self.__absolute_file_path, "rb") as file_object:
+                tags = exifread.process_file(file_object)
+
+                if "Image DateTimeOriginal" in tags:
+                    date_time_original = tags["Image DateTimeOriginal"]
+                elif "Image DateTime" in tags:
+                    date_time_original = tags["Image DateTime"]
+
+                if (
+                        "GPS GPSLatitude" in tags and "GPS GPSLongitude" in tags and
+                        "GPS GPSLatitudeRef" in tags and "GPS GPSLongitudeRef" in tags
+                ):
+                    latitude = tags["GPS GPSLatitude"]
+                    longitude = tags["GPS GPSLongitude"]
+                    latitude_ref = tags["GPS GPSLatitudeRef"]
+                    longitude_ref = tags["GPS GPSLongitudeRef"]
+        except OSError as error:
+            logging.debug(f"Error reading exif data from {self.__absolute_file_path}: {error}")
+        except Exception as error:
+            logging.error(f"Error reading exif data from {self.__absolute_file_path}: {error}")
+
+        self.__exif = Exif(
+            date_time_original=date_time_original,
+            latitude=latitude,
+            longitude=longitude,
+            latitude_ref=latitude_ref,
+            longitude_ref=longitude_ref
+        )
